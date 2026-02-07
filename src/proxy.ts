da6835281a2f4f5288dcf0286a6767b74f45e4b8
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-07 12:56:14 +0530

    issue of vercel fixed with login1

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/api/webhooks(.*)',
+  '/api/uploadthing(.*)',
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
